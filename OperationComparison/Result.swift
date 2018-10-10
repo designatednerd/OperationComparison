f39a3fcc498b8dc89e0a9915197e287fb3100db9
Ellen Shapiro <designatednerd@gmail.com>
2018-10-10 21:44:08 +0200

    Add RxSwift and timer stuff

--- OperationComparison/Result.swift
+++ OperationComparison/Result.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import PromiseKit
+import RxSwift
 
 enum Result<T> {
     case success(_ item: T)
@@ -45,3 +46,38 @@ extension Result {
     }
 }
 
+
+// MARK: - RxSwift Helpers
+
+extension Result {
+    
+    func finishSingle(_ single: (SingleEvent<T>) -> Void) {
+        switch self {
+        case .success(let item):
+            single(.success(item))
+        case .error(let error):
+            single(.error(error))
+        }
+    }
+    
+    static func singleFromFunction(_ function: @escaping (@escaping ResultCompletion<T>) -> Void) -> Single<T> {
+        return Single.create { observer in
+            function { result in
+                result.finishSingle(observer)
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    static func singleFromFunction<U>(with parameter: U,
+                                      _ function: @escaping (U, @escaping ResultCompletion<T>) -> Void) -> Single<T> {
+        return Single.create { observer in
+            function(parameter) { result in
+                result.finishSingle(observer)
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
--- OperationComparison/ViewController.swift
+++ OperationComparison/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import PromiseKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
@@ -24,6 +26,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction private func loadAndProcess() {
+        let start = Date()
         imageView.image = nil 
         activityIndicator.startAnimating()
         operationLabel.text = "Fetching user..."
@@ -44,7 +47,7 @@ class ViewController: UIViewController {
                             switch resizedImageResult {
                             case .success(let resizedImage):
                                 self.imageView.image = resizedImage
-                                self.operationLabel.text = "Complete!"
+                            self.operationLabel.text = "Complete in \(self.formattedSecondsSince(start))!"
                             case .error(let error):
                                 self.operationLabel.text = "Error resizing image: \(error)"
                             }
@@ -64,33 +67,42 @@ class ViewController: UIViewController {
     // MARK: - PromiseKit
     
     @IBAction private func loadAndProcessPromiseKit() {
+        let start = Date()
         activityIndicator.startAnimating()
         imageView.image = nil
-        operationLabel.text = "Fetching user..."
+        operationLabel.text = "Fetching user (PromiseKit)..."
 
         /// https://github.com/mxcl/PromiseKit/blob/master/Documentation/FAQ.md#do-i-need-to-worry-about-retain-cycles
         Result.promiseFromFunction(FakeAPI.fetchUser)
-            .then { [weak self] user -> Promise<UIImage> in
+            .map { [weak self] user in
                 guard let self = self else { throw GenericError.selfWasDeallocated }
+                self.operationLabel.text = "Fetching image (PromiseKit)..."
                 
-                self.operationLabel.text = "Fetching image..."
-                return Result.promiseFromFunction(with: user, RealAPI.fetchImage)
+                return user
             }
-            .then { [weak self] image -> Promise<UIImage> in
+            .then { user in
+                Result.promiseFromFunction(with: user, RealAPI.fetchImage)
+            }
+            .map { [weak self] image in
                 guard let self = self else { throw GenericError.selfWasDeallocated }
                 
-                self.operationLabel.text = "Resizing Image..."
-                return self.resizeImagePromise(with: image)
+                self.operationLabel.text = "Resizing Image (PromiseKit)..."
+                return image
+            }
+            .then { image in
+                self.resizeImagePromise(with: image)
             }
             .ensure { [weak self] in
                 self?.activityIndicator.stopAnimating()
             }
             .done { [weak self] resizedImage in
-                self?.imageView.image = resizedImage
-                self?.operationLabel.text = "Done!"
+                guard let self = self else { return }
+                
+                self.imageView.image = resizedImage
+                self.operationLabel.text = "Complete (PromiseKit) in \(self.formattedSecondsSince(start))!"
             }
             .catch { [weak self] error in
-                self?.operationLabel.text = "Error occurred: \(error)"
+                self?.operationLabel.text = "Error occurred (PromiseKit): \(error)"
             }
     }
     
@@ -102,13 +114,82 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: - RxSwift
+    
+    private let bag = DisposeBag()
+    
+    @IBAction private func loadAndProcessRxSwift() {
+        let start = Date()
+        activityIndicator.startAnimating()
+        imageView.image = nil
+        operationLabel.text = "Fetching user (RxSwift)..."
+        
+        let image = Result.singleFromFunction(FakeAPI.fetchUser)
+            .map { user in
+                self.updateOperationLabel(with: "Fetching image (RxSwift)...", result: user)
+            }
+            .flatMap { user in
+                Result.singleFromFunction(with: user, RealAPI.fetchImage)
+            }
+            .map { image in
+                self.updateOperationLabel(with: "Resizing image (RxSwift)...", result: image)
+            }
+            .flatMap { image in
+                self.resizeImageSingle(with: image)
+            }
+        
+        let running = Observable
+            .from([
+                image.map { _ in false }.asObservable()
+                ])
+            .merge()
+            .startWith(true)
+            .asDriver(onErrorJustReturn: false)
+        
+        
+        image
+            .subscribe(
+                onSuccess: { [weak self] resizedImage in
+                    guard let self = self else { return }
+                    
+                    self.imageView.image = resizedImage
+                    self.operationLabel.text = "Complete (RxSwift) in \(self.formattedSecondsSince(start))!"
+                },
+                onError: { [weak self] error in
+                    self?.operationLabel.text = "Error occurred (RxSwift): \(error)"
+                }
+            )
+            .disposed(by: bag)
+        
+        running
+            .drive(activityIndicator.rx.isAnimating)
+            .disposed(by: bag)
+    }
+    
+    private func updateOperationLabel<T>(with text: String, result: T) -> T {
+        self.operationLabel.text = text
+        return result
+    }
+    
+    private func resizeImageSingle(with image: UIImage) -> Single<UIImage> {
+        return Single.create { observer in
+            
+            ImageResizer.resizeImage(image, to: self.imageView.frame.size) { result in
+                result.finishSingle(observer)
+            }
+            return Disposables.create()
+        }
+    }
+    
     // MARK: - Custom Operator
     
     @IBAction private func loadAndProcessCustomOperator() {
+        let start = Date()
         activityIndicator.startAnimating()
         imageView.image = nil
-        
-        let chain = fetchUser
+        operationLabel.text = "Fetching user (Custom Operator)..."
+
+        let chain = FakeAPI.fetchUser
             --> fetchUserImage
             --> resizeImageForImageView
         
@@ -120,26 +201,25 @@ class ViewController: UIViewController {
             switch result {
             case .success(let image):
                 self.imageView.image = image
-                self.operationLabel.text = "Complete!"
+                self.operationLabel.text = "Complete (Custom Operator) in \(self.formattedSecondsSince(start))!"
             case .error(let error):
-                self.operationLabel.text = "Error occurred: \(error)"
+                self.operationLabel.text = "Error occurred (Custom Operator): \(error)"
             }
         }
     }
     
-    private func fetchUser(completion: @escaping (Result<User>) -> Void) {
-        operationLabel.text = "Fetching user..."
-        FakeAPI.fetchUser(completion: completion)
-    }
-    
     private func resizeImageForImageView(_ image: UIImage, completion: @escaping (Result<UIImage>) -> Void) {
-        operationLabel.text = "Resizing image..."
+        operationLabel.text = "Resizing image (Custom Operator)..."
         ImageResizer.resizeImage(image, to: self.imageView.frame.size, completion: completion)
     }
     
     private func fetchUserImage(for user: User, completion: @escaping (Result<UIImage>) -> Void) {
-        operationLabel.text = "Fetching image..."
+        operationLabel.text = "Fetching image (Custom Operator)..."
         RealAPI.fetchImage(for: user, completion: completion)
+    }
+    
+    private func formattedSecondsSince(_ date: Date) -> String {
+        return String(format: "%.3f", Date().timeIntervalSince(date)) + "s"
     }
 }
 
