7cd845f5b78213efc87403da7d4b7c9a79223369
Ellen Shapiro <designatednerd@gmail.com>
2018-10-10 14:39:25 +0200

    Initial Commit

---
+++ OperationComparison/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OperationComparison
+//
+//  Created by Ellen Shapiro on 10/10/18.
+//  Copyright © 2018 Bakken & Baeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
