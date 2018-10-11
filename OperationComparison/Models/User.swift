4aae24592762c5c6776d0fa888f562671bb35d5e
Ellen Shapiro <designatednerd@gmail.com>
2018-10-11 11:55:31 +0200

    add some alternate images for funsies

--- OperationComparison/Models/User.swift
+++ OperationComparison/Models/User.swift
@@ -11,7 +11,14 @@ struct User {
     let imageURL: String
     
     static var fakeUser: User {
+        let randomImages = [
+            "https://bakkenbaeck.com/images/offices/covers/amsterdam@2x.2305df1e81.jpg",
+            "https://bakkenbaeck.com/images/offices/covers/bonn@2x.4ef90789e3.jpg",
+            "https://bakkenbaeck.com/images/offices/covers/oslo@2x.277cee31f9.jpg",
+            "https://bakkenbaeck.com/images/offices/covers/sanfrancisco@2x.1b6aee123d.jpg",
+        ]
+        
         return User(name: "Fakey McFakerson",
-                    imageURL: "https://bakkenbaeck.com/images/offices/covers/amsterdam@2x.2305df1e81.jpg")
+                    imageURL: randomImages.randomElement()!)
     }
 }
