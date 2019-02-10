0382f470db68a8511f662a83ba4e2ae35707a927
Ellen Shapiro <designatednerd@gmail.com>
2019-02-10 14:26:50 +0100

    update images

--- OperationComparison/Models/User.swift
+++ OperationComparison/Models/User.swift
@@ -11,11 +11,12 @@ struct User {
     let imageURL: String
     
     static var fakeUser: User {
+        // Let's grab some random crap from my instagram!
         let randomImages = [
-            "https://bakkenbaeck.com/images/offices/covers/amsterdam@2x.2305df1e81.jpg",
-            "https://bakkenbaeck.com/images/offices/covers/bonn@2x.4ef90789e3.jpg",
-            "https://bakkenbaeck.com/images/offices/covers/oslo@2x.277cee31f9.jpg",
-            "https://bakkenbaeck.com/images/offices/covers/sanfrancisco@2x.1b6aee123d.jpg",
+            "https://scontent-ams3-1.cdninstagram.com/vp/66b86ccbfddbe6fc6f980b0ea440d61b/5CE4C6D5/t51.2885-15/e35/43578459_246261869378716_6922717545703145472_n.jpg?_nc_ht=scontent-ams3-1.cdninstagram.com",
+            "https://scontent-ams3-1.cdninstagram.com/vp/91969563d8deecdf797a45319aecc028/5CE3993D/t51.2885-15/e35/41163396_284088662207726_3012594673932828672_n.jpg?_nc_ht=scontent-ams3-1.cdninstagram.com",
+            "https://scontent-ams3-1.cdninstagram.com/vp/8fe6b39c6d189e31702c2a162ec37128/5CEA7E7C/t51.2885-15/e35/35998742_1946966825593833_3371460546091024384_n.jpg?_nc_ht=scontent-ams3-1.cdninstagram.com",
+            "https://scontent-ams3-1.cdninstagram.com/vp/9a3e1a9ebf8f9ce192431dad26766c8a/5CFB5395/t51.2885-15/e35/29715184_2039690519622232_2028413419249467392_n.jpg?_nc_ht=scontent-ams3-1.cdninstagram.com",
         ]
         
         return User(name: "Fakey McFakerson",
